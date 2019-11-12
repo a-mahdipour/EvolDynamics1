52a7d5003cbd0d6b26bccfe587c5cc3a6ddd6c38
Ali Mahdipour-Shirayeh <ali.mahdipour@gmail.com>
2019-11-12 15:16:24 -0500

    Update timevariationBD4CompSD.m

--- timevariationBD4CompSD.m
+++ timevariationBD4CompSD.m
@@ -1,7 +1,15 @@
+
+
+#######################################################
+###    Analytic calculation of Fixation Probablity  ###
+###               in 4-compartment model            ### 
+###    University of Waterloo, Waterloo,Canada      ###
+###            copyright@AliMahdipour               ###
+#######################################################
+
+
 clear
 clc
-%tic
-
 tmax=2000;
 iteration=200000;
 
@@ -35,43 +43,11 @@ for i=1:1:iteration
         
         var1(i,t+1)=XX;
         var2(i,t+1)=YY;
-                
-%         tt=t;
-%         if(XX==0 && YY==0 || XX==Ns &&  YY==Nd)
-%           for ii=tt+1:1:tmax
-%             var1(1,ii)=XX;
-%             var2(1,ii)=YY;
-%           end
-%           t=tmax+1;
-%         end
-
-% tt=t;
-%         if(XX==0 || XX==Ns )
-%           for ii=tt+1:1:tmax
-%             var1(1,ii)=XX;
-%           end
-%           t=tmax+1;
-%         end
-%         
-%         
-%         if(YY==0||YY==Nd)
-%            for ii=tt+1:1:tmax
-%             var2(1,ii)=YY;
-%            end
-%            t=tmax+1;
-%         end
-%              
-        
-        
         x = XX;
         y = YY;
-       
-       
+    
     end
-    
-    
-    
-   
+ 
  end
 
 
@@ -79,22 +55,7 @@ for i=1:1:iteration
   for kk=1:tmax
      Var1(1,kk) = (1/Ns)* (1/iteration)*sum(var1(:,kk));
      Var2(1,kk) = (1/Nd)* (1/iteration)*sum(var2(:,kk));
-%      Var3(1,kk) = 1-Var1(1,kk);
-%      Var4(1,kk) = 1-Var2(1,kk);
-%      
   end
-
-% (1-(uu/(1-uu)))/(1-(uu/(1-uu))^N)
-% Var1(1,:) =(1/N)* (1/iteration)*sum(var1,1);
-% Var2(1,:) =(1/N)*(1/iteration)*sum(var2,1);
-
-% for ttt=1:tmax
-%     if(abs(var1(1,ttt)-var(1,tmax)) < 0.0000001)
-%         TTT = 0.02*tmax+ttt
-%     break    
-%         
-%     end
-% end
 
 Var1(1,tmax)
 Var2(1,tmax)
@@ -113,20 +74,3 @@ figure(2)
 plot(qq,Var2,'y','LineWidth',2);
 xlabel('Time'); ylabel('Number of Mutant Stem/Progenitor/Differentited Cells')
 
-
-% figure(1) 
-% plot(qq,Var2,'b','LineWidth',2);
-% xlabel('Time'); ylabel('Number of Mutant Stem/Progenitor/Differentited cells')
-% 
-% figure(1) 
-% plot(qq,Var4,'k','LineWidth',2);
-% xlabel('Time'); ylabel('Number of Mutant Stem/Progenitor/Differentited cells')
-% title('4-Compartment BD Model,r_1= r^~_1=1, r_2=1.5,r^~_2=1,u_1 = 0.5,u_2 = 0.2,\eta_1 = \eta_2 = 0.01,N_S=10,N_D=100')
-
-
-
-% figure(1) 
-% plot(qq,Var3,'g','LineWidth',2);
-% xlabel('Time'); ylabel('Number of Stem/Progenitor/Differentited cells')
-
-%toc
